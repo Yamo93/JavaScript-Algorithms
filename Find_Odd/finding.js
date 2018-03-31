db05d18fbab7fbda2fa2fa5c3bbe9d888b703438
Yamo93 <yamo.gebrewold@gmail.com>
2018-03-31 13:34:46 +0300

    added extra solutions

--- Find_Odd/finding.js
+++ Find_Odd/finding.js
@@ -27,3 +27,27 @@ function findOdd(A) {
 
   console.log(findOdd([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]));
   console.log(findOdd([1,1,2,-2,5,2,4,4,-1,-2,5]));
+  console.log(findOdd([10], 10));
+  console.log(findOdd([1,1,1,1,1,1,10,1,1,1,1]));
+  console.log(findOdd([5,4,3,2,1,5,4,3,2,10,10]));
+
+  // the shortest solution with bitwise operators
+  function findOddTwo(A) {
+  
+    return A.reduce((l, r) => l ^ r);
+  }
+
+  console.log(findOddTwo([1,1,1,1,1,1,10,1,1,1,1]));
+  console.log(findOddTwo([5,4,3,2,1,5,4,3,2,10,10]));
+
+  // a similar solution with bitwise operators
+  function findOddThree(A) {
+      var out = 0;
+      for (var i = 0; i<A.length; i++) {
+        out = out ^ A[i];
+      }
+      return out;
+  }
+
+  console.log(findOddThree([1,1,1,1,1,1,10,1,1,1,1]));
+  console.log(findOddThree([5,4,3,2,1,5,4,3,2,10,10]));
